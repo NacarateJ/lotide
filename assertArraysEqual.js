6e5835fb56625def5eee924a052110394aa28a7f
Jaqueline <jaquelinenacarate@gmail.com>
2023-03-27 01:57:23 +0000

    created assertion function that makes use of eqArrays function foor array comparision and takes two arrays and console.log an appropriate message to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  // call the eqArrays function with the actual and expected arrays as arguments to check if they are equal.
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([], []);
+assertArraysEqual([1], [1]);
+assertArraysEqual([1], [2]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]);
+
