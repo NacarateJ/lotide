80d8285d53957a3d0d72da3debafd5d56f7ee0a2
Jaqueline <jaquelinenacarate@gmail.com>
2023-03-30 19:40:01 +0000

    added some comments

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,9 @@
+// Create a function called countLetters.
+
+// The function should take in a sentence (as a string) and then return a
+// count of each of the letters in that sentence skipping whitespaces
+
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -29,16 +35,18 @@ const countLetters = function(string) {
   return result;
 }
 
-console.log(countLetters("hello world"));
+// console.log(countLetters("hello world"));
 
-// const result1 = countLetters("hello world");
-// assertEqual(result1["h"], 1); // Test should pass: There is only 1 "h" in "hello world"
-// assertEqual(result1["e"], 1); // Test should pass: There is only 1 "e" in "hello world"
-// assertEqual(result1["l"], 3); // Test should pass: There are 3 "l"s in "hello world"
-// assertEqual(result1["o"], 2); // Test should pass: There are 2 "o"s in "hello world"
-// assertEqual(result1["w"], 1); // Test should pass: There is only 1 "w" in "hello world"
-// assertEqual(result1["r"], 1); // Test should pass: There is only 1 "r" in "hello world"
-// assertEqual(result1["d"], 1); // Test should pass: There is only 1 "d" in "hello world"
-// assertEqual(result1[" "], undefined); // Test should pass: Spaces are not counted in the result object
-// assertEqual(result1["x"], undefined); // Test should pass: "x" is not present in "hello world"
+
+// TEST CASE USING ASSERTEQUAL
+const result1 = countLetters("hello world");
+assertEqual(result1["h"], 1); // Test should pass: There is only 1 "h" in "hello world"
+assertEqual(result1["e"], 1); // Test should pass: There is only 1 "e" in "hello world"
+assertEqual(result1["l"], 3); // Test should pass: There are 3 "l"s in "hello world"
+assertEqual(result1["o"], 2); // Test should pass: There are 2 "o"s in "hello world"
+assertEqual(result1["w"], 1); // Test should pass: There is only 1 "w" in "hello world"
+assertEqual(result1["r"], 1); // Test should pass: There is only 1 "r" in "hello world"
+assertEqual(result1["d"], 1); // Test should pass: There is only 1 "d" in "hello world"
+assertEqual(result1[" "], undefined); // Test should pass: Spaces are not counted in the result object
+assertEqual(result1["x"], undefined); // Test should pass: "x" is not present in "hello world"
 
