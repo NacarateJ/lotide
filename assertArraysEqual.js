a90ac6b6e1cf5a9ebe21d3bf1f9adbf8a897a640
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-01 22:32:12 +0000

    added notes with expected return

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,14 +20,14 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([], []);
-assertArraysEqual([1], [1]);
-assertArraysEqual([1], [2]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([1, 2], [1, 2, 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // FAIL
+assertArraysEqual([], []); // PASS
+assertArraysEqual([1], [1]); // PASS
+assertArraysEqual([1], [2]); // FAIL
+assertArraysEqual([1, 2, 3], [1, 2]); // FAIL
+assertArraysEqual([1, 2], [1, 2, 3]); // FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // FAIL
 
