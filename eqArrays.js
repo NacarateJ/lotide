0f01acfdac91647ad8c81975ab3870574ae0af55
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-15 00:07:29 +0000

    refactoring eqArrays using modules to share code between other files, test file created for test code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -18,14 +10,5 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1], [1]), true);
-assertEqual(eqArrays([1], [2]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+module.exports = eqArrays;
+
