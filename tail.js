94e04c7dd270c236cdc22c7baf3f66930c224b53
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-15 19:48:53 +0000

    fixed tail to return an array insted of a string

--- tail.js
+++ tail.js
@@ -1,9 +1,14 @@
 // TAIL FUNCTION TO WORK WITH TEST TO CHECK THE IF NEM ARRAY IS EXCLUDING THE HEAD (i[0]) OF THE ARRAY
-// USING to.String() METHOD
+
 const tail = (array) => {
-  return array.slice(1).join(", ");
+  return array.slice(1);
 };
 
 module.exports = tail;
+
+console.log(tail([5, 6, 7]));
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+console.log(tail([7]));
+console.log(tail([]));
 
 
