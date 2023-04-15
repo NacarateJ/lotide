df252b8e951d6b256d80747e046c73a15e8c6364
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-15 21:09:22 +0000

    adjusted the test code for tail to check if returns an array insted of a string

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,17 +3,17 @@ const tail = require("../tail");
 
 
 describe("#tail", () => {
-  it("returns '6, 7' for [5, 6, 7]", () => {
-    assert.deepEqual(tail([5, 6, 7]), '6, 7');
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
   });
-  it("returns 'Lighthouse, Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), 'Lighthouse, Labs');
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
-  it("returns '' for [7]", () => {
-    assert.deepEqual(tail([7]), '');
+  it("returns [] for [7]", () => {
+    assert.deepEqual(tail([7]), []);
   });
-  it("returns '' for []", () => {
-    assert.deepEqual(tail([]), '');
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
   });
 });
 
